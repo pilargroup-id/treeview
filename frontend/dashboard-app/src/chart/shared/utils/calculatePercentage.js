667102fea3efefd164edb37add02ce11ba5c9675
alfatih-frontendpiagam-beep <frontend.piagam@gmail.com>
2025-12-11 09:08:41 +0700

    123

--- frontend/dashboard-app/src/chart/shared/utils/calculatePercentage.js
+++ frontend/dashboard-app/src/chart/shared/utils/calculatePercentage.js
@@ -1,6 +1,3 @@
-/**
- * Menghitung persentase perubahan antara dua nilai
- */
 export const calculatePercentageChange = (val2024, val2025) => {
   if (val2024 === 0) {
     return val2025 > 0 ? 100 : 0;
