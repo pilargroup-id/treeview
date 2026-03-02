b18f7d497488d3b55cb052b560c1240eab448940
alfatih-frontendpiagam-beep <frontend.piagam@gmail.com>
2026-03-02 09:36:15 +0700

    delete unimportant files

--- frontend/dashboard-app/src/chart/shared/utils/calculatePercentage.js
+++
@@ -1,7 +0,0 @@
-export const calculatePercentageChange = (val2024, val2025) => {
-  if (val2024 === 0) {
-    return val2025 > 0 ? 100 : 0;
-  }
-  return ((val2025 - val2024) / val2024) * 100;
-};
-
